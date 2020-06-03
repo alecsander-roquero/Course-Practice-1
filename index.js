3bf1171aee76f9b79e29a078526a838155b5d085
a021 <alecs_roquero@yahoo.com>
2020-06-03 18:45:48 +0800

    Added passport code to index.js

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
-
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: 'mememememe',
+    clientSecret: 'ewewewewewe',
+    callbackURL: 'auth/google/callback'
+},  (accessToken) => {
+        console.log(accessToken);
+}
+));
 
 app.get('/', (req,res) => {
     res.send({course: 'practice'});
     
 });
 
-const PORT =process.env.PORT || 5000;
+app.get('/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
 
