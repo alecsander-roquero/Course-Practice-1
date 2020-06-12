2cee25131395a1ea95ff2eba5c4fc070901a92b7
a021 <alecs_roquero@yahoo.com>
2020-06-12 19:39:22 +0800

    Added route for /auth/google/callback

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,15 +2,20 @@ const passport = require('passport');
 
 module.exports = (app) => {
 
-app.get('/', (req,res) => {
-    res.send({course: 'practice'});
-    
-});
+    app.get('/', (req,res) => {
+        res.send({course: 'practice'});
+        
+    });
 
-app.get('/auth/google', 
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
+    app.get('/auth/google', 
+        passport.authenticate('google', {
+            scope: ['profile', 'email'],
+            //successRedirect: '/auth/google/callback'
+        })
+    );
+
+    app.get('/auth/google/callback', passport.authenticate('google', {
+        scope: ['profile', 'email'],
     })
-);
-
+    );
 }
--- services/passport.js
+++ services/passport.js
@@ -5,7 +5,9 @@ passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-},  (accessToken) => {
+},  (accessToken, refreshToken, profile, done) => {
         console.log(accessToken);
+        console.log(refreshToken);
+        console.log(profile);
 }
 ));
