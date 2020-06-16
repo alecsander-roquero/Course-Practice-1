841a382fbe385399c6c0b87e42a4fa40d7892778
a021 <alecs_roquero@yahoo.com>
2020-06-16 13:34:37 +0800

    Added user model

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 require('passport');
 
 require('./services/passport');
 require('./routes/authRoutes')(app);
 
-
+mongoose.connect(keys.mongoURI);
 
 const PORT = process.env.PORT || 5000;
 
