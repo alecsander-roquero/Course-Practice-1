8dde0584e83bcc2009b2ca40083b5fb66ce7ed86
a021 <alecs_roquero@yahoo.com>
2020-06-17 09:30:13 +0800

    Added cookie session to app

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
-require('passport');
+const cookieSession = require('cookie-session');
+
+const passport = require('passport');
 
 require('./models/User');
 require('./services/passport');
 require('./routes/authRoutes')(app);
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: keys.cookieKeys
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(keys.mongoURI);
 
