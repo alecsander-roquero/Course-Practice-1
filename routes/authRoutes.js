434cc9178bc276c3041bd9d72d04f9f582fa7b7f
a021 <alecs_roquero@yahoo.com>
2020-06-18 15:41:33 +0800

    Fixing Authentication #2

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -19,11 +19,13 @@ module.exports = (app) => {
     }));
 
     app.get('/api/current_user', (req, res) => {
+        console.log(req.user);
         res.send(req.user);
     });
 
     app.get('/api/logout', (req, res) => {
         req.logout();
+        
         res.send(req.user);
     });
 }
