fdbe5ca4d8c93236a8d5693b5c7bcd86371da6f8
a021 <alecs_roquero@yahoo.com>
2020-06-18 13:18:41 +0800

    Added dev file

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    googleClientID: '490853720421-dard0j1mcqp490ah3g2j741r505jgbrc.apps.googleusercontent.com',
+    googleClientSecret: 'Pqv8I9jMiWNmyCvqzqZqM-dl',
+    mongoURI: 'mongodb+srv://admin:OqUSui9WxrSOAVwk@cluster0-94itc.mongodb.net/userDB?retryWrites=true&w=majority',
+    cookieKeys: 'ireallystillloveyoubutimusttletyougobecauseitistherightthingtododontworryiwillnotbotheryouanymorejustbehappy'
+}
+//shimi
+//
+//mongodb+srv://shimi:cpE7daK1CSyxMvgV@cluster0-r5ycr.mongodb.net/userDB?retryWrites=true&w=majority
