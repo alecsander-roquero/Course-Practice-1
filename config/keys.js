da78543ab35b3c146c4b1a6fb2f4422333b3e405
a021 <alecs_roquero@yahoo.com>
2020-06-18 14:48:00 +0800

    Try fix v1

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV === 'production'){
     //we are in production
-    module.exports = require('prod.js');
+    module.exports = require('./prod.js');
 }else{
     //we are in dev't
-    module.exports = require('dev.js');
+    module.exports = require('./dev.js');
 }
