62184f2c13d31d7306c71216dd08745cb0c4f5c3
a021 <alecs_roquero@yahoo.com>
2020-06-18 14:06:14 +0800

    Added dev and prod keys

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if(process.env.NODE_ENV === 'production'){
+    //we are in production
+    module.exports = require('prod.js');
+}else{
+    //we are in dev't
+    module.exports = require('dev.js');
+}
