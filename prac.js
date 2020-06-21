e81ed30e0c9d9ca4298914ce79e4ebf24b63fbc9
a021 <alecs_roquero@yahoo.com>
2020-06-21 11:06:50 +0800

    Added practice file

---
+++ prac.js
@@ -0,0 +1,16 @@
+function fetchAlbums(){
+    fetch('rallycoding.herokuapp.com/api/music_albums')
+    .then(res => res.json())
+    .then(json => console.log(json));
+}
+async function fetchAlbums(){
+    const res = await fetch('rallycoding.herokuapp.com/api/music_albums');
+    const json = await res.json();
+    console.log(json);
+}
+
+const fetchAlbums = async() => {
+    const res = await fetch('rallycoding.herokuapp.com/api/music_albums');
+    const json = await res.json();
+    console.log(json);
+}
