cc8757adc7f59fa7e5818c01cebd892330b23ae8
a021 <alecs_roquero@yahoo.com>
2020-06-24 20:00:11 +0800

    Added css to client

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import 'materialize-css/dist/css/materialize.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -8,6 +9,6 @@ import reducers from './reducers';
 const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
-    <Provider store={store}><App/></Provider>, 
+    <Provider store={store}><App /></Provider>, 
     document.querySelector('#root')
 );
