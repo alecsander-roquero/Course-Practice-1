978ddb9813b39b00faf840d35762e0e4b5b807e8
a021 <alecs_roquero@yahoo.com>
2020-06-26 15:17:37 +0800

    Added fetchUser to app

--- client/actions/index.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-import { FETCH_USER } from './types';
-const fetchUser = () => {
-    axios.get('/api/current_user')
-    .then(res => dispatch({ type: FETCH_USER, payload: res}));
-}
---
+++ client/src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+ export const fetchUser = () => 
+    (dispatch) => {
+        axios.get('/api/current_user')
+        .then(res => dispatch({ type: FETCH_USER, payload: res}));
+    };
+
+
+/* function mapDispatchToProps(dispatch, state) {
+    return { 
+        xxxxxActions: fetchUser,
+        fetchUser: function () {
+            dispatch( axios.get('/api/current_user')
+            .then(res => dispatch({ type: FETCH_USER, payload: res})));
+        },
+
+    };
+} */
